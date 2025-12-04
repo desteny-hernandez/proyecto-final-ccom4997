7be2faae677e4893c2e18fa9d4506b4566923f23
michelle-angelic <michelle.santiago19@upr.edu>
2025-12-04 16:02:54 +0000

    second try at cmake

---
+++ build/CMakeFiles/inverted.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/inverted.dir/main.cpp.o"
+  "CMakeFiles/inverted.dir/main.cpp.o.d"
+  "inverted"
+  "inverted.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/inverted.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
